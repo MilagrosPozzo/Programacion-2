085953fcb1798bfc248738052f96034f549ef217
MilagrosPozzo <milagrospozzofasini@gmail.com>
2025-05-11 11:17:44 -0300

    Commit 1

--- Ejercicios_1001_problemas/src/Problema_441.java
+++ Ejercicios_1001_problemas/src/Problema_441.java
@@ -1,13 +1,29 @@
-public class Main {
+package javaapplication6;
+
+/**
+ *
+ * @author Milagros Pozzo
+ */
+public class Problema_441 {
+
     public static void main(String[] args) {
-        // Creamos un arreglo de enteros con 5 elementos.
+        /*
+         * PROBLEMAS EN EL CÓDIGO ORIGINAL:
+         * - El arreglo 'numeros' tiene solo 5 elementos (índices válidos: 0 a 4).
+         * - El bucle for usaba 'i < 10', lo que lleva a intentar acceder a índices inválidos como numeros[5], numeros[6], etc.
+         * - Esto genera una excepción ArrayIndexOutOfBoundsException en tiempo de ejecución.
+         * - También, la cadena "" "" en el System.out.print es inválida (doble comilla seguida de espacio y otra doble comilla).
+         *
+         * SOLUCIÓN:
+         * - Cambiar la condición del for a 'i < numeros.length' para limitarse a los índices válidos del arreglo.
+         * - Corregir la cadena para que tenga un espacio adecuado: " " en vez de "" "".
+         */
+
         int[] numeros = {1, 2, 3, 4, 5};
 
-        // Usamos un ciclo for para recorrer el arreglo.
-        // Usamos "numeros.length" para evitar acceder a índices inválidos.
-        // Si usáramos "i < 10", se intentaría acceder a posiciones que no existen, causando un error.
+        // Recorremos el arreglo usando la longitud del mismo
         for (int i = 0; i < numeros.length; i++) {
-            // Imprimimos cada elemento seguido de un espacio
+            // Imprime cada número seguido de un espacio
             System.out.print(numeros[i] + " ");
         }
     }
