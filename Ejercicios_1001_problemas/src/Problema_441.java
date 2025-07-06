1e46b8047be2ae30c4fb3bc5902e7f5006446f26
MilagrosPozzo <milagrospozzofasini@gmail.com>
2025-07-06 11:08:15 -0300

    Commit 1

--- Ejercicios_1001_problemas/src/Problema_441.java
+++ Ejercicios_1001_problemas/src/Problema_441.java
@@ -8,6 +8,9 @@ public class Problema_441 {
 
     public static void main(String[] args) {
         /*
+        Descripción: El siguiente código intenta imprimir los elementos de un arreglo de enteros, pero accede a posiciones inválidas.
+int[] numeros = {1, 2, 3, 4, 5};
+for (int i = 0; i < 10; i++) { System.out.print(numeros[i] + "" ""); } [/code]
          * PROBLEMAS EN EL CÓDIGO ORIGINAL:
          * - El arreglo 'numeros' tiene solo 5 elementos (índices válidos: 0 a 4).
          * - El bucle for usaba 'i < 10', lo que lleva a intentar acceder a índices inválidos como numeros[5], numeros[6], etc.
