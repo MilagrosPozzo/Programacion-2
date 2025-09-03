a73f9138245a70e67e084878dfadbb2cd747ce2b
MilagrosPozzo <milagrospozzofasini@gmail.com>
2025-09-03 16:50:05 -0300

    Commit 2

---
+++ FrioExpressProyecto/FrioExpress/src/frioexpress/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package frioexpress;
+
+/**
+ *
+ * @author Milagros Pozzo
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a FrioExpress");
+        
+        // Aquí puedes crear objetos de tus clases
+        // Ejemplo:
+        // Cliente cliente = new Cliente("Juan Pérez");
+        // Producto producto = new Producto("Helado", 100);
+        // Pedido pedido = new Pedido(cliente, producto, 2);
+        // SistemaFrioExpress sistema = new SistemaFrioExpress();
+        // sistema.procesarPedido(pedido);
+    }
+}
